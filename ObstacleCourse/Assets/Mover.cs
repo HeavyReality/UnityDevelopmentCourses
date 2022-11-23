6d1b1445efbedd83c22026b2b9bcd05531153cd9
Rob Forbes <deepfoxdev@gmail.com>
2022-11-22 19:10:25 -0600

    Moving code to methods

--- ObstacleCourse/Assets/Mover.cs
+++ ObstacleCourse/Assets/Mover.cs
@@ -10,14 +10,28 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        //Translates the position of the player based on the Horizontal/Vertical inputs
         float xValue = Input.GetAxis("Horizontal") * movementFactor * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * movementFactor * Time.deltaTime;
         transform.Translate(xValue,0,zValue);
+    }
+
+    void PrintInstructions()
+    {
+        //Print instructions to the log
+        Debug.Log("This is a comment");
+        Debug.Log("Move the player with the keyboard");
+        Debug.Log("Try not to break through the walls!");
     }
 }
