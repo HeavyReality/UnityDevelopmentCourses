df17409bf1c4bbade22e73f85bef5340f796b8f4
Rob Forbes <deepfoxdev@gmail.com>
2022-11-30 01:45:43 -0600

    Add upward thrust to rocket and prep rotation

--- Project Rocket/Assets/Scripts/Movement.cs
+++ Project Rocket/Assets/Scripts/Movement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody moveRigidbody;
+    [SerializeField] float thrustFactor = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveRigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust(){
+        if(Input.GetKey(KeyCode.Space)){
+            Debug.Log("Pressed Space - Thrusting");
+            moveRigidbody.AddRelativeForce( Vector3.up * thrustFactor * Time.deltaTime);
+        }
+    }
+    void ProcessRotation(){
+
+        if((Input.GetKey(KeyCode.A)) && !(Input.GetKey(KeyCode.D))){
+            Debug.Log("Rotate Left");
+        }
+
+        if(!(Input.GetKey(KeyCode.A)) && (Input.GetKey(KeyCode.D))){
+            Debug.Log("Rotate Right");
+        }
     }
 }
