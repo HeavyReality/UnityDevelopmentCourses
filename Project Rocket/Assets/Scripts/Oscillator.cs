d5b6f3e37edda5a3f43605aadf2cdcc5edac44ee
Rob Forbes <deepfoxdev@gmail.com>
2023-03-04 15:24:03 -0600

    Automate movement through sin waves!

--- Project Rocket/Assets/Scripts/Oscillator.cs
+++ Project Rocket/Assets/Scripts/Oscillator.cs
@@ -8,6 +8,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         Vector3 offset = movementVector * movementFactor;
-         transform.position = startingPosition + offset;
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin( cycles * tau );
+        movementFactor = Mathf.Abs(rawSinWave);
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
 
     }
 }
